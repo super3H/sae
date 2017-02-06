50610161c57bd58b5e6346730493ec99cc77977d
super3H <781176645@qq.com>
2017-02-06 11:40:26 +0800

    'test0.11'

--- weixinInterface.py
+++ weixinInterface.py
@@ -33,12 +33,17 @@ class WeixinInterface:
 		if hashcode == signature:
 			return echostr
 			
-	def POST(self):        
+	def POST(self): 
 		str_xml = web.data() #获得post来的数据
 		xml = etree.fromstring(str_xml)#进行XML解析
-		content = xml.find("Content").text#获得用户所输入的内容
 		msgType = xml.find("MsgType").text
 		fromUser = xml.find("FromUserName").text
 		toUser = xml.find("ToUserName").text
-		return self.render.reply_text(fromUser,toUser,int(time.time()),content)
+		if msgType == 'text':
+			content = xml.find("Content").text
+			return self.render.reply_text(fromUser,toUser,int(time.time()), content)
+		elif msgType == 'image':
+			pass
+		else:
+			pass
 			
