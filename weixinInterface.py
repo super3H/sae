50cb7757f6d0dc016ec6f8c2118547dee8d1305e
super3H <781176645@qq.com>
2017-02-06 11:18:02 +0800

    'test0.9'

--- weixinInterface.py
+++ weixinInterface.py
@@ -34,7 +34,7 @@ class WeixinInterface:
 			return echostr
 			
 	def POST(self):        
-        str_xml = web.data() #获得post来的数据
+		str_xml = web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行XML解析
         content=xml.find("Content").text#获得用户所输入的内容
         msgType=xml.find("MsgType").text
