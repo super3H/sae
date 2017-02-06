76564ed0adfe9fe3b940f6315fab3103e6414eda
super3H <781176645@qq.com>
2017-02-06 15:39:35 +0800

    'test0.14'

--- imgtest.py
+++ imgtest.py
@@ -7,25 +7,25 @@ def imgtest(picurl):
 	url = 'http://how-old.net/Home/Analyze?isTest=False&source=&version=001'
 	header = {
 	'Accept-Encoding':'gzip, deflate',
-    'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
-    'Host': "how-old.net",
-    'Referer': "http://how-old.net/",
-    'X-Requested-With': "XMLHttpRequest"
-    }
+	'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
+	'Host': "how-old.net",
+	'Referer': "http://how-old.net/",
+	'X-Requested-With': "XMLHttpRequest"
+	}
 	#此处打开picurl文件
 	data = {'file':s.get(picurl).content}
-	
+
 	r = s.post(url, files=data, headers=header)
 	h = r.content
 	i = h.replace('\\','')
-	
+
 	gender = re.search(r'"gender": "(.*?)"rn', i)
 	age = re.search(r'"age": (.*?),rn', i)
 	if gender.group(1) == 'Male':
-        gender1 = '男'
-    else:
-        gender1 = '女'
-    datas = [gender1, age.group(1)]
+		gender1 = '男'
+	else:
+		gender1 = '女'
+	datas = [gender1, age.group(1)]
 	return datas
 	
 	
