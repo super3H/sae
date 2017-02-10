e6eac1c12056ed737d493c3828790a58eda660ce
super3H <781176645@qq.com>
2017-02-10 18:10:20 +0800

    'test0.23'

--- tulingAutoReply.py
+++ tulingAutoReply.py
@@ -26,7 +26,7 @@ class TulingAutoReply:
                 return js['url']
             elif js['code'] == 302000:
 			    articles = js['list']
-                articleList = [article['article']+article['detailurl'] for article in articles]
+                articleList = [article['article']+article['detailurl'] for article in articles ]
                 return choice(articleList)
 			# ÆäËû
             else:
