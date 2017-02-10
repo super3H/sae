c3e1cd8dcf058f39e3e9b05ce8046f8cbfe87c68
super3H <781176645@qq.com>
2017-02-10 18:30:58 +0800

    'test0.23'

--- tulingAutoReply.py
+++ tulingAutoReply.py
@@ -26,7 +26,7 @@ class TulingAutoReply:
                 return js['url']
             elif js['code'] == 302000:
 			    return js['list']
-			elif code == 308000:
+			elif js['code'] == 308000:
 			    return j['text']+j['list'][0]['info']+j['list'][0]['detailurl']
 			# ÆäËû
             else:
