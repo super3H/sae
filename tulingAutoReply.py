b00e0c36987970f6fd3b2b748d3cda18f6e16e3b
super3H <781176645@qq.com>
2017-02-10 18:07:11 +0800

    'test0.23'

--- tulingAutoReply.py
+++ tulingAutoReply.py
@@ -26,9 +26,7 @@ class TulingAutoReply:
                 return js['url']
             elif js['code'] == 302000:
 			    articles = js['list']
-                articleList = ['']
-                for article in articles:
-                    articleList.append(article['article']+u'\n详情：'+article['detailurl'])
+                articleList = [article['article']+'\n详情：'.decode('gbk')+article['detailurl'] for article in articles]
                 return choice(articleList)
 			# 其他
             else:
