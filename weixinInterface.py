2a478e092584374345e1550dd0610bee3340987e
super3h <781176645@qq.com>
2017-08-12 19:37:26 +0800

    更新音乐链接

--- weixinInterface.py
+++ weixinInterface.py
@@ -48,16 +48,16 @@ class WeixinInterface:
 				return self.render.reply_text(fromUser,toUser,int(time.time()),replayText)
 			if content.lower().strip() == 'm':
 				musicList = [
-					[r'http://sz-btfs.ftn.qq.com/ftn_handler/2cc23e8dafb31c35363a3c39e900dd9fe5c235b50762faf1b49705d267d422678ddad67f258f3752bcc4653324d9390e532079cb42c84711fbae78da7d3bbe54/%E8%B5%B5%E9%9B%B7-%E6%88%90%E9%83%BD.mp3','成都'.decode('gbk'),'一首关于成都的民谣'.decode('gbk')],
-					[r'http://sz-ctfs.ftn.qq.com/ftn_handler/7b58ec7a523887d4d9284f234a4d0061c64771af32b95f2dfbc61b12a1c514881e119bab54f60c136fab41934676f70438741bc3d72216ab9bbad5267c65bd92/%E5%A5%87%E5%A6%99%E8%83%BD%E5%8A%9B%E6%AD%8C%20%E9%99%88%E7%B2%92.mp3','奇妙能力歌'.decode('gbk'),'我有那么多奇妙的能力，却留不住你'.decode('gbk')],
-					[r'http://sz-btfs.ftn.qq.com/ftn_handler/648b012faa45729a791d71cc18814ff10d4a68a1bc01b3124d6375d5491e7caacc2074a5da08f2cbca9efd32f520f1b709dc241d5cb197a3db1557f0fe5fd7ec/?fname=%E4%B8%83%E6%9C%88%E4%B8%8A.mp3','七月上'.decode('gbk'),'我欲乘风破浪，相敬流年，不负相识一场'.decode('gbk')],
-					[r'http://sh-btfs.ftn.qq.com/ftn_handler/bac6bdd51934a94c90dadcb4301dcb65f83c599d7d41b3ab5709397d1f03724701f7ef0ffbcde9deec7e771b736de5365432d05b6b18c690f3081abf83ba2f5f/%E5%88%B0%E4%B8%8D%E4%BA%86.mp3','到不了'.decode('gbk'),'心这个东西.如果先伤了别人的 总有一天会加倍伤回来的..放心 谁也跑不了'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/a0f18ebed344549f629d1a838b8fe6ed0d163a7336e195991fc0ad9a688a59ea2ca9b848cd122a5b3102b2c68b559cdb37d229777165520db5e8f1a8f5d16657/%E5%8D%97%E4%B8%8B.mp3','南下'.decode('gbk'),'生来北方人，不知江南心'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/c47cedaa54f1624a6b7c8e942b9e0bd1b75e78d8768c973d917ee79035f61a5fb2da4b187a5e42aa142e6b52f3648916793ec04197b9626139c4f35dd2c0d502/%E6%88%BF%E9%97%B4.mp3','房间'.decode('gbk'),'在这温暖的房间 我们都笑得很甜 一切停格在一瞬'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/929758c4e674b77f31eacd501125be223cab72f4f7799154cfcb99b2935f17833854aad7675a871ad1e2943b2c56a788d2a46e8126b462abf446e61bf4961ec6/%E5%91%8A%E7%99%BD%E6%B0%94%E7%90%83.mp3','告白气球'.decode('gbk'),'向喜欢的Ta告白吧'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/a929de2e9ad0301d8fbe153f573ecb80e02e0e82d6da7ef1468c6f3cd1da3699a19068a0a3e3e5aeaf902622a12a615ca211eccf98f8e3fb4433480aeed0692c/%E6%88%91%E8%A6%81%E4%BD%A0.mp3','我要你'.decode('gbk'),'我想要更好更圆的月亮， 想要未知的疯狂， 想要声色的张扬， 我想要你。'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/fe4aead7483bb2dda36488796938aa25541d45a5102c0740372dc4f45659b10332c3baf1a08918eca22cc4234fc646a384a4e8e5aa715a23ff4177ea8f8f629d/%E6%98%A5%E5%A4%8F%E7%A7%8B%E5%86%AC%E7%9A%84%E4%BD%A0.mp3','春夏秋冬的你'.decode('gbk'),'我喜欢 春天的花 夏天的树 秋天的黄昏 冬天的阳光 和 每天的你'.decode('gbk')],
-					[r'http://sh-ctfs.ftn.qq.com/ftn_handler/c26f1616ce6d42e98bb44053500b34ce831ebfd768d67a65a2229dc90a3af6539d65adc70bf66609d26b01215b0b0c099d2f4735a54b82fbc01187d50238e3d1/%E7%88%B1%E6%83%85.mp3','爱情'.decode('gbk'),'爱是折磨人的东西,却又舍不得这样放弃'.decode('gbk')]
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E8%B5%B5%E9%9B%B7%20-%20%E6%88%90%E9%83%BD.mp3','成都'.decode('gbk'),'一首关于成都的民谣'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E5%A5%87%E5%A6%99%E8%83%BD%E5%8A%9B%E6%AD%8C%20%E9%99%88%E7%B2%92.mp3','奇妙能力歌'.decode('gbk'),'我有那么多奇妙的能力，却留不住你'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E4%B8%83%E6%9C%88%E4%B8%8A%20-%20Jam.mp3','七月上'.decode('gbk'),'我欲乘风破浪，相敬流年，不负相识一场'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E6%9D%8E%E4%BB%A3%E6%B2%AB%20-%20%E5%88%B0%E4%B8%8D%E4%BA%86.mp3','到不了'.decode('gbk'),'心这个东西.如果先伤了别人的 总有一天会加倍伤回来的..放心 谁也跑不了'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E5%BE%90%E6%B5%B7%E4%BF%8F%20-%20%E5%8D%97%E4%B8%8B.mp3','南下'.decode('gbk'),'生来北方人，不知江南心'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E5%88%98%E7%91%9E%E7%90%A6%20-%20%E6%88%BF%E9%97%B4.mp3','房间'.decode('gbk'),'在这温暖的房间 我们都笑得很甜 一切停格在一瞬'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E4%B8%87%E5%B2%81%E7%88%BA%20-%20%E7%88%B1%E6%83%85.mp3','告白气球'.decode('gbk'),'向喜欢的Ta告白吧'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E4%BB%BB%E7%B4%A0%E6%B1%90%20-%20%E6%88%91%E8%A6%81%E4%BD%A0.mp3','我要你'.decode('gbk'),'我想要更好更圆的月亮， 想要未知的疯狂， 想要声色的张扬， 我想要你。'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E7%8E%8B%E5%AE%87%E8%89%AF%2B-%2B%E6%98%A5%E5%A4%8F%E7%A7%8B%E5%86%AC%E7%9A%84%E4%BD%A0.mp3','春夏秋冬的你'.decode('gbk'),'我喜欢 春天的花 夏天的树 秋天的黄昏 冬天的阳光 和 每天的你'.decode('gbk')],
+					[r'http://collegeshare.oss-cn-qingdao.aliyuncs.com/weixin/music/%E4%B8%87%E5%B2%81%E7%88%BA%20-%20%E7%88%B1%E6%83%85.mp3','爱情'.decode('gbk'),'爱是折磨人的东西,却又舍不得这样放弃'.decode('gbk')]
 				
 				]
 				music = random.choice(musicList)
